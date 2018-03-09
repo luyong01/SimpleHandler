9719027e8b11f42842f1d0cd86bc79878d093af1
ranze <luyong1107@gmail.com>
2018-03-09 20:42:08 +0800

    Initial commit

---
+++ app/src/main/java/com/ranze/simplehandlerdemo/MainActivity.java
@@ -0,0 +1,17 @@
+package com.ranze.simplehandlerdemo;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ranze on 2018/3/9.
+ */
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+}
